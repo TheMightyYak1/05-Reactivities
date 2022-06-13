dd77edfe3f2f985b3adfb66cc0e6af3ab4cae04e
Brendan Baker <bren.r.baker@gmail.com>
2022-06-13 14:41:31 +1000

    End of Section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,7 +13,11 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
-
+using MediatR;
+using Application.Activities;
+using Application.Core;
+using AutoMapper;
+using API.Extensions;
 
 namespace API
 {
@@ -33,21 +37,7 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
-            services.AddCors(opt =>
-            {
-                opt.AddPolicy("CorsPolicy", policy =>
-                {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                });
-            });
+            services.AddApplicationServices(_config);
   
         }
 
